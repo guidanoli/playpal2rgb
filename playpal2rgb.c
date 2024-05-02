6fba51e830bb96071335e4f24584aaa5cfd53290
Guilherme Dantas <guidanoli@proton.me>
2024-05-02 12:32:28 -0300

    Read palette in RGBX format

--- playpal2rgb.c
+++ playpal2rgb.c
@@ -5,13 +5,16 @@
 int read_palette(FILE* palette_fp, uint8_t palette[256][3]) {
     int c;
     for (int i = 0; i < 256; ++i) {
-        for (int j = 0; j < 3; ++j) {
+        for (int j = 0; j < 4; ++j) {
             c = fgetc(palette_fp);
             if (c == EOF) {
                 fprintf(stderr, "Palette file is too small\n");
                 return EXIT_FAILURE;
             } else {
-                palette[i][j] = (uint8_t)c;
+                // Ignore every fourth byte
+                if (j < 3) {
+                    palette[i][j] = (uint8_t)c;
+                }
             }
         }
     }
