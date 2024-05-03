b0848acf0139d639d567bab48ac93792a59580aa
Guilherme Dantas <guidanoli@proton.me>
2024-05-03 00:54:24 -0300

    Organize code

--- playpal2rgb.c
+++ playpal2rgb.c
@@ -2,60 +2,129 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int read_palette(FILE* palette_fp, uint8_t palette[256][3]) {
-    int c;
-    for (int i = 0; i < 256; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            c = fgetc(palette_fp);
-            if (c == EOF) {
-                fprintf(stderr, "Palette file is too small\n");
+static const char* usage_message =
+"Convert images from PLAYPAL to RGB8888 format\n"
+"\n"
+"Usage: playpal2rgb <palette>\n"
+"\n"
+"Reads image in PLAYPAL format from stdin\n"
+"Writes image in RGB888 format to stdout\n"
+"Palette file should be in RGBX8888 format\n"
+"\n"
+"For more information, visit https://github.com/guidanoli/playpal2rgb\n";
+
+static int read_palette_from_file
+(
+        FILE* palette_file,
+        uint8_t palette[256][3]
+)
+{
+    for (int i = 0; i < 256; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            int c = fgetc(palette_file);
+
+            if (c == EOF)
+            {
+                fputs("Palette file is too small\n", stderr);
                 return EXIT_FAILURE;
-            } else {
+            }
+            else
+            {
                 // Ignore every fourth byte
-                if (j < 3) {
+                if (j < 3)
+                {
                     palette[i][j] = (uint8_t)c;
                 }
             }
         }
     }
-    if (fgetc(palette_fp) != EOF) {
-        fprintf(stderr, "Palette file is too big\n");
+
+    if (fgetc(palette_file) != EOF)
+    {
+        fputs("Palette file is too big\n", stderr);
         return EXIT_FAILURE;
     }
+
+    if (ferror(palette_file))
+    {
+        perror("Error reading palette file");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
 
-int apply_palette(uint8_t const palette[256][3]) {
+static int read_palette_from_filepath
+(
+    const char* palette_filepath,
+    uint8_t palette[256][3]
+)
+{
+    FILE* palette_file = fopen(palette_filepath, "rb");
+
+    if (palette_file == NULL)
+    {
+        perror("Error opening palette file");
+        return EXIT_FAILURE;
+    }
+
+    int ret = read_palette_from_file(palette_file, palette);
+
+    fclose(palette_file);
+
+    return ret;
+}
+
+static int apply_palette
+(
+    uint8_t const palette[256][3]
+)
+{
     int c;
-    while ((c = getchar()) != EOF) {
-        for (int i = 0; i < 3; ++i) {
-            if (putchar(palette[c][i]) == EOF) {
+
+    while ((c = getchar()) != EOF)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            if (putchar(palette[c][i]) == EOF)
+            {
                 perror("Error writing to output");
                 return EXIT_FAILURE;
             }
         }
     }
-    if (ferror(stdin)) {
+
+    if (ferror(stdin))
+    {
         perror("Error reading from input");
         return EXIT_FAILURE;
     }
+
     return EXIT_SUCCESS;
 }
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        fprintf(stderr, "Expected palette file path\n");
+int main
+(
+    int argc,
+    const char* argv[]
+)
+{
+    if (argc < 2)
+    {
+        fputs(usage_message, stderr);
         return EXIT_FAILURE;
     }
+
     uint8_t palette[256][3];
-    FILE* palette_fp = fopen(argv[1], "rb");
-    if (palette_fp == NULL) {
-        perror("Error opening palette file");
-        return EXIT_FAILURE;
+
+    int ret = read_palette_from_filepath(argv[1], palette);
+
+    if (ret != EXIT_SUCCESS)
+    {
+        return ret;
     }
-    int ret;
-    ret = read_palette(palette_fp, palette);
-    fclose(palette_fp);
-    if (ret != EXIT_SUCCESS) { return ret; }
+
     return apply_palette(palette);
 }
